5f77db61581ee56e12c4d08f264b74e110626b67
mikiencolor <mrosariokling@outlook.com>
2023-09-22 17:54:59 +0200

    Added fetch_hn_entries test of HTTPError handling.

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,4 +1,6 @@
+import pytest
 from unittest.mock import patch, Mock
+from requests.exceptions import HTTPError
 from tests.constants import *
 from tests.utils import *
 from src.models.hn_scraper import HackerNewsScraper
@@ -17,6 +19,7 @@ def test_fetch_hn_entries():
 
     Note: This test assumes that the sample HTML structure remains consistent with the current version of the YCombinator news page.
     If the structure of the YCombinator news page changes, this test may fail and the sample HTML (and possibly the scraper logic) will need to be updated.
+    This test also assumes that fetch_hn_entries is called by the HackerNewsScraper class constructor on instantiation.
     """
     # Arrange
     mock_html_content = get_mocked_hn_html()
@@ -37,3 +40,22 @@ def test_fetch_hn_entries():
     assert len(entries) == expected_num_entries, f"Expected {expected_num_entries} entries, but got {len(entries)}"
     for i, (actual_entry, expected_entry) in enumerate(zip(entries, MOCK_HTML_EXPECTED_RETURN_VALUE)):
         assert actual_entry == expected_entry, f"Entry at index {i} does not match expected value. Got {actual_entry}, expected {expected_entry}"
+
+def test_fetch_hn_entries_http_error():
+    """
+    Test fetch_hn_entries when an HTTP error occurs.
+    The function should handle the error and not crash.
+    Note: This test assumes that fetch_hn_entries is called by the HackerNewsScraper class constructor on instantiation.
+    """
+    # Arrange
+    expected_num_entries = MOCK_HTML_HN_ENTRIES_NUM_TO_FETCH
+
+    # Mock the requests.get method to raise an HTTPError
+    with patch('src.models.hn_scraper.requests.get', side_effect=HTTPError("Mocked HTTP Error")):
+        HackerNewsScraper._instance = None
+        try:
+            # Act
+            HackerNewsScraper(max_entries=expected_num_entries)
+        # Assert
+        except HTTPError:
+            pytest.fail("The method did not handle the HTTPError.")
