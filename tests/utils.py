8dc4e8808f0240a6eb56c36b2593a03b5ac346a6
mikiencolor <mrosariokling@outlook.com>
2023-09-22 13:43:33 +0200

    Moved get_mocked_hn_html helper function to test.utils.

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+from tests.constants import MOCK_HTML_FILE_RELATIVE_PATH
 from src.models.hn_entry import HackerNewsEntry
 
 # Auxiliary Functions
@@ -14,3 +17,9 @@ def validate_hnentry_list_type(entries: any) -> bool:
     if not isinstance(entries, list):
         return False
     return all(isinstance(entry, HackerNewsEntry) for entry in entries)
+
+def get_mocked_hn_html() -> str:
+    current_file_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+    mock_file_path = current_file_dir / MOCK_HTML_FILE_RELATIVE_PATH
+    with open(mock_file_path, "r", encoding="utf-8") as file:
+        return file.read()
